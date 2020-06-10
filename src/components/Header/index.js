54c7f3081a410b047a0d40dc650410bdef06a028
Igor Oliveira <igorgarciaco@gmail.com>
2020-06-10 10:58:46 -0300

    navbar ajust

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import { Link } from 'gatsby';
 import './style.css';
 import {
+  Container,
   NavDropdown,
-  Form,
   Navbar,
-  FormControl,
   Nav,
-  Button,
 } from 'react-bootstrap';
 
 import Logo from "./logo.js"
@@ -18,20 +16,18 @@ class Header extends Component {
     super(props);
     this.toggle = this.toggleNavbar.bind(this);
 
-  
-
     // https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
     if (typeof window !== 'undefined') {
 
       window.addEventListener("scroll", function (event) {
         if (window.pageYOffset > 100) {
-          document.getElementById('navbar').style.background = "rgba(0,0,0,0.8)";
+          document.getElementById('navbar').classList.add("navbar-fixed");
         }
         else {
-          document.getElementById('navbar').style.background = "transparent";
+          document.getElementById('navbar').classList.remove("navbar-fixed");
         }
       });
-      
+
       let prevScrollpos = window.pageYOffset;
       window.onscroll = function () {
         const maxScroll = document.body.clientHeight - window.innerHeight;
@@ -66,28 +62,29 @@ class Header extends Component {
     return (
       <React.Fragment>
         <Navbar expand="lg" id="navbar" className="main-navbar">
-          <Navbar.Brand href="#home">
-            <Logo />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="inside-nav">
-              <Nav.Link href="#home">Inicio</Nav.Link>
-              <Nav.Link href="#aboutSection">Sobre</Nav.Link>
-              <NavDropdown title="Servicos" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#assessoria">Ativa despacho</NavDropdown.Item>
-                <NavDropdown.Item href="#apoema">apoema ceritificações</NavDropdown.Item>
-                <NavDropdown.Item href="#contabilidade">ativa contabilidade</NavDropdown.Item>
-                <NavDropdown.Item href="#logistica">ativa logistica</NavDropdown.Item>
-                <NavDropdown.Item href="#juridico">ativa juridico</NavDropdown.Item>
-                <NavDropdown.Item href="#keyWest">key west transportes</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#units">Unidades</Nav.Link>
-              <Nav.Link href="#contact">Contato</Nav.Link>
-              <Nav.Link href="#link">Noticias</Nav.Link>
-
-            </Nav>
-          </Navbar.Collapse>
+          <Container>
+            <Navbar.Brand href="#home">
+              <Logo />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="inside-nav">
+                <Nav.Link href="#home">Inicio</Nav.Link>
+                <Nav.Link href="#aboutSection">Sobre</Nav.Link>
+                <NavDropdown title="Servicos" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#assessoria">Ativa despacho</NavDropdown.Item>
+                  <NavDropdown.Item href="#apoema">apoema ceritificações</NavDropdown.Item>
+                  <NavDropdown.Item href="#contabilidade">ativa contabilidade</NavDropdown.Item>
+                  <NavDropdown.Item href="#logistica">ativa logistica</NavDropdown.Item>
+                  <NavDropdown.Item href="#juridico">ativa juridico</NavDropdown.Item>
+                  <NavDropdown.Item href="#keyWest">key west transportes</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#units">Unidades</Nav.Link>
+                <Nav.Link href="#contact">Contato</Nav.Link>
+                <Nav.Link href="#link">Noticias</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
         </Navbar>
       </React.Fragment>
     );
