7d452a79de79b89b8a5422e3a005017e74bc80e4
Igor Oliveira <igorgarciaco@gmail.com>
2020-08-31 09:35:26 -0300

    type writter home

--- src/components/CoverText/index.js
+++ src/components/CoverText/index.js
@@ -6,12 +6,16 @@ import "./style.css"
 
 const CoverText = () => {
 
+    var ReactRotatingText = require('react-rotating-text');
+
     return (
         <Container>
             <Row>
                 <Col>
                     <div className="cover-text" id="coverText">
-                        <p className="cover-title text-uppercase">Não oferecemos<br /> serviços, oferecemos <br /> <span className="we-offer">segurança.</span></p>
+                        <p className="cover-title text-uppercase">
+                            Não oferecemos<br /> serviços, oferecemos <br /> <span className="we-offer"><ReactRotatingText cursor={false} items={['first', 'second', 'third']} />.</span>
+                        </p>
                         {/* <p className="cover-subtitle">O Grupo Ativa reúne varios serviços interligados que se fundem para<br /> lhe oferecer um só resultado; a sua satisfação!</p> */}
                     </div>
                 </Col>
